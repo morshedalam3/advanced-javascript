54afc09f0454c2b90d02fda2880aa870270153b7
morshedalam3 <morshedhasan1969@gmail.com>
2021-02-02 13:06:01 +0600

    true or false list

--- true-false.js
+++ true-false.js
@@ -1,3 +1,17 @@
+//falsy
+//0
+//""
+//false
+//undefined
+//NaN
+//null
+//truthy
+// num>0
+//true
+//" "
+//"false"
+//[]
+//"0"
 const number = 34;
 if(number > 0){
     console.log("condition is true")
