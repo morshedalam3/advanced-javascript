65c842b2ffd1ee8d652e474ee1a81142e96a378b
morshedalam3 <morshedhasan1969@gmail.com>
2021-02-02 14:25:17 +0600

    null vs undefiend

---
+++ null-vs-undefiend.js
@@ -0,0 +1,14 @@
+let pakhi;
+function add (num1,num2){
+console.log(num1 + num2);
+return
+}
+function add (num1,num2){
+    console.log(num1, num2);
+}
+const premik = {name:"jobo", phone: 898509, address: "lalPur",}
+console.log(premik.gfname)
+let fun = undefined;
+console.log(fun)
+const age = [23, 34, 34, 44,]
+console.log(age[11]);
