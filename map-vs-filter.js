a617422aeb8067128a350657dd59e03c09435954
morshedalam3 <morshedhasan1969@gmail.com>
2021-02-02 18:50:24 +0600

    map-find-filter

--- map-vs-filter.js
+++ map-vs-filter.js
@@ -3,17 +3,19 @@ const output = [];
 for(let i = 0; i < number.length; i++){
     const element = number[i];
     const result = element * element;
+
     output.push(result); 
-} or line 8-9
-function square(element){
-    return element * element;
-} or line 12-13
-const result = number.map(function square(element){
-    return element * element;
-}) or line 14
-const result = number.map(element => element * element); or line 15
-const result = number.map(x => x * x); 
-const result = number.filter(x => x > 4); (filter give an Array)
-const result = number.find(x => x > 1) (give first number )
+}
+
+// function square(element){
+//     return element * element;
+// } or line 12-13
+// const result = number.map(function square(element){
+//     return element * element;
+// }) or line 14
+// const result = number.map(element => element * element); or line 15
+// const result = number.map(x => x * x); 
+// const result = number.filter(x => x > 4); (filter give an Array)
+// const result = number.find(x => x > 1) (give first number )
 
 console.log(result);
