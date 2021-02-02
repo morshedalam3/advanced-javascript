86973940b25b21c85bdf0074498e54712e87e525
morshedalam3 <morshedhasan1969@gmail.com>
2021-02-02 20:07:16 +0600

    scope

---
+++ scope.js
@@ -0,0 +1,19 @@
+const bonus = 20;
+function sum(first, second){
+    const result = first + second + bonus;
+    if( result > 10){
+        var mood = "happy"
+        mood = "funny";
+        mood = "cool";
+        console.log(mood);
+    }
+    // console.log(bonus)
+    // console.log(mood); (error dekabe , cause if er bayre teke dekayce ty)
+    // return result;
+
+}
+const output = sum(7, 5)
+// console.log(bonus)
+// console.log(output);
+//const or let hole if er vitoer tekt log korte hobe;
+// var hole error dekabe na
